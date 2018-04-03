868ca3d5f24b42e0c7c07cc17f631b432cc2d344
notgiven688 <webminerpool@protonmail.com>
2018-04-03 20:58:01 +0200

    cleaning

--- server/Server/Firewall.cs
+++ server/Server/Firewall.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿// The MIT License (MIT)
+
+// Copyright (c) 2018 - the webminerpool developer
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
@@ -32,12 +53,14 @@ namespace Server {
 			public DateTime FirstSeen = DateTime.Now;
 		}
 
-		private static ConcurrentDictionary<string, Entry> entries = new ConcurrentDictionary<string, Entry> ();
+		private static CcDictionary<string, Entry> entries = new CcDictionary<string, Entry> ();
 
 		public const int CheckTimeInHeartbeats = 6 * 10; //  every 10min
 
-		private static void AddToIpTables (string ip) {
-			Helper.WriteTextAsyncWrapper ("ip_list", ip + Environment.NewLine);
+		private static void AddToIpTables (Entry entry, int rule) {
+			Helper.WriteTextAsyncWrapper ("ip_list", entry.Address + Environment.NewLine);
+			Console.WriteLine ("Added {0} to iptables (rule #{1})", entry.Address, rule.ToString ());
+			entries.TryRemove (entry.Address);
 		}
 
 		public static void Update (string ip, UpdateEntry update) {
@@ -61,38 +84,24 @@ namespace Server {
 		}
 
 		public static void Heartbeat (int heartBeats) {
-			Entry dummy;
 
 			List<Entry> entrylst = new List<Entry> (entries.Values);
+
 			foreach (Entry entry in entrylst) {
 				// decide here...
 				if (entry.AuthSuccess == 0 && entry.SolvedJobs == 0 &&
 					entry.AuthFailure > 20) {
-
-					AddToIpTables (entry.Address);
-					entries.TryRemove (entry.Address, out dummy);
-					Console.WriteLine ("Added {0} to iptables (rule #1)", entry.Address);
-
+					AddToIpTables (entry, 1);
 				} else if (entry.AuthFailure > 500 && entry.AuthSuccess < 500) {
-					AddToIpTables (entry.Address);
-					entries.TryRemove (entry.Address, out dummy);
-					Console.WriteLine ("Added {0} to iptables (rule #2)", entry.Address);
+					AddToIpTables (entry, 2);
 				} else if (entry.AuthSuccess + entry.AuthFailure > 1000 && entry.SolvedJobs < 3) {
-					AddToIpTables (entry.Address);
-					entries.TryRemove (entry.Address, out dummy);
-					Console.WriteLine ("Added {0} to iptables (rule #3)", entry.Address);
+					AddToIpTables (entry, 3);
 				} else if (entry.AuthSuccess + entry.AuthFailure > 4000) {
-					AddToIpTables (entry.Address);
-					entries.TryRemove (entry.Address, out dummy);
-					Console.WriteLine ("Added {0} to iptables (rule #4)", entry.Address);
+					AddToIpTables (entry, 4);
 				} else if (entry.WrongHash > 0 && entry.AuthSuccess < 5) {
-					AddToIpTables (entry.Address);
-					entries.TryRemove (entry.Address, out dummy);
-					Console.WriteLine ("Added {0} to iptables (rule #5)", entry.Address);
+					AddToIpTables (entry, 5);
 				} else if (entry.AuthSuccess + entry.AuthFailure > 2000 && entry.Handshake < 1) {
-					AddToIpTables (entry.Address);
-					entries.TryRemove (entry.Address, out dummy);
-					Console.WriteLine ("Added {0} to iptables (rule #6)", entry.Address);
+					AddToIpTables (entry, 6);
 				}
 			}
 
