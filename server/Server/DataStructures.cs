f016b44aa6bb26ae79cac8091dea6da79bc6fdb1
notgiven688 <webminerpool@protonmail.com>
2018-04-03 12:58:11 +0200

    refactor

--- server/Server/DataStructures.cs
+++ server/Server/DataStructures.cs
@@ -17,14 +17,13 @@ namespace Server
 
     public class CcQueue<T> : ConcurrentQueue<T>
     {
-
     }
 
     public class CcHashset<T>
     {
         ConcurrentDictionary<T, byte> dictionary = new ConcurrentDictionary<T, byte>();
 
-        public bool Add(T item)
+        public bool TryAdd(T item)
         {
             return dictionary.TryAdd(item, byte.MaxValue);
         }
@@ -33,6 +32,8 @@ namespace Server
         {
             get { return dictionary.Keys; }
         }
+
+        public int Count { get { return dictionary.Count; }}
 
         public bool Contains(T item)
         {
