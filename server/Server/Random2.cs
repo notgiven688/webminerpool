197c66c077b6d110153e173eeecd4c5696355fb2
wmpadmin <webminerpool@protonmail.com>
2018-04-10 14:01:19 +0200

    added thread safe random

---
+++ server/Server/Random2.cs
@@ -0,0 +1,52 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2018 - the webminerpool developer
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+using System;
+
+namespace Server
+{
+
+	// System.Random is not thread safe(!!)
+	// https://blogs.msdn.microsoft.com/pfxteam/2009/02/19/getting-random-numbers-in-a-thread-safe-way/
+
+	public static class Random2 
+	{ 
+		private static Random global = new Random(); 
+
+		[ThreadStatic] 
+		private static Random local;
+
+		public static double NextDouble()
+		{
+			Random inst = local; 
+			if (inst == null) 
+			{ 
+				int seed; 
+				lock (global) seed = global.Next(); 
+				local = inst = new Random(seed); 
+			} 
+			return inst.NextDouble (); 
+		}
+			
+	}
+}
+
