f965b513468f56848025fc135c3076372d09ff0c
wmpadmin <webminerpool@protonmail.com>
2018-04-21 15:47:57 +0200

    getting closer

--- hash_cn/libhash/main.c
+++ hash_cn/libhash/main.c
@@ -7,36 +7,38 @@
 
 #include "slow-hash.h"
 
-  
-char* hash_cn(char* hex, int light, int variant)
+char *hash_cn(char *hex, int light, int variant)
 {
-     char* output = (char*) malloc((64+1)*sizeof(char));
-  
-     int len = strlen(hex)/2;
-     
-     unsigned char val[len], *pos = hex;
-     
-     for(size_t count = 0; count < len; count++)  {
-         sscanf(pos, "%2hhx", &val[count]);
-         pos += 2;
-     }
-     
-    //int variant = ((const uint8_t*)val)[0] >= 7 ? ((const uint8_t*)val)[0] - 6 : 0;
+    char *output = (char *)malloc((64 + 1) * sizeof(char));
+
+    int len = strlen(hex) / 2;
+
+    unsigned char val[len], *pos = hex;
+
+    for (size_t count = 0; count < len; count++)
+    {
+        sscanf(pos, "%2hhx", &val[count]);
+        pos += 2;
+    }
+
+    if (variant == -1)
+        variant = ((const uint8_t *)val)[0] >= 7 ? ((const uint8_t *)val)[0] - 6 : 0;
 
     unsigned char hash[32];
 
     cn_slow_hash(&val, len, &hash, light, variant, 0);
 
     char *ptr = &output[0];
-    
-    for (size_t i = 0; i < 32; i++) {
-	ptr += sprintf (ptr, "%02x",hash[i]);
+
+    for (size_t i = 0; i < 32; i++)
+    {
+        ptr += sprintf(ptr, "%02x", hash[i]);
     }
 
     return &output[0];
 }
 
-void hash_free(void* ptr)
+void hash_free(void *ptr)
 {
     free(ptr);
 }
