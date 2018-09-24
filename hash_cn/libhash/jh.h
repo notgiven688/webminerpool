c4c07301883fe11205c08021f1ec40faf099a975
wmpadmin <webminerpool@protonmail.com>
2018-09-24 16:34:05 +0200

    cn_v2.2 initial commit

--- hash_cn/libhash/jh.h
+++ hash_cn/libhash/jh.h
@@ -1,21 +1,16 @@
-/*This program gives the 64-bit optimized bitslice implementation of JH using ANSI C
+#ifndef JH_H
+#define JH_H
+#include <stdint.h>
 
-   --------------------------------
-   Performance
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-   Microprocessor: Intel CORE 2 processor (Core 2 Duo Mobile T6600 2.2GHz)
-   Operating System: 64-bit Ubuntu 10.04 (Linux kernel 2.6.32-22-generic)
-   Speed for long message:
-   1) 45.8 cycles/byte   compiler: Intel C++ Compiler 11.1   compilation option: icc -O2
-   2) 56.8 cycles/byte   compiler: gcc 4.4.3                 compilation option: gcc -O3
+extern void jh(unsigned bit_len, const uint8_t input[],
+    size_t input_bit_length, uint8_t output[]);
 
-   --------------------------------
-   Last Modified: January 16, 2011
-*/
-#pragma once
+#ifdef __cplusplus
+}
+#endif
 
-typedef unsigned char BitSequence;
-typedef unsigned long long DataLength;
-typedef enum {SUCCESS = 0, FAIL = 1, BAD_HASHLEN = 2} HashReturn;
-
-HashReturn jh_hash(int hashbitlen, const BitSequence *data, DataLength databitlen, BitSequence *hashval);
+#endif
