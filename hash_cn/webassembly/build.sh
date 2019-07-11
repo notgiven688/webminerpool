d8a17a9343c97ffef9c684768eba5374dedc590f
PiTi <deagle_r0ck@web.de>
2019-07-11 19:40:32 +0200

    No need

--- hash_cn/webassembly/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-emcc *.c argon2/*.c -O3 \
-    -s SINGLE_FILE=1 \
-    -s 'EXTRA_EXPORTED_RUNTIME_METHODS=["ccall", "cwrap"]' \
-    --llvm-lto 1 \
-    -s WASM=1 \
-    -s "BINARYEN_TRAP_MODE='allow'" \
-    -s EXPORTED_FUNCTIONS="['_hash_cn']" \
-    -o cryptonight.js
